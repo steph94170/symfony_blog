b1c3d9ae50f22878e364709efa34515218321551
stephane <stephanekoffi@live.fr>
2024-05-20 14:42:47 +0200

    spint3_ticket2_feat: l'admin peut gerer les category

--- src/Controller/Admin/Category/CategoryController.php
+++ src/Controller/Admin/Category/CategoryController.php
@@ -62,4 +62,47 @@ class CategoryController extends AbstractController
         ]);
     }
 
+    #[Route('/category/{id<\d+>}/edit', name: 'admin_category_edit', methods: ['GET', 'POST'])]
+    public function edit(Category $category, Request $request): Response 
+    {   
+        $form = $this->createForm(AdminCategoryFormType::class, $category);
+       $form ->handleRequest($request);
+        
+       if ($form->isSubmitted()&& $form->isValid() ){
+
+        $category->setUpdeatedAt(new DateTimeImmutable());
+
+       $this->em->persist($category);
+       $this->em->flush();
+       $this->addFlash("success", "La catégorie {$category->getName()}a été modifiée");
+       return $this->redirectToRoute('admin_category_index');
+
+    }
+
+        return $this->render("pages/admin/category/edit.html.twig" ,[
+            "form"=>$form->createView(),
+            "category" => $category
+    
+    
+    ]);
+
+        
+    }
+
+    #[Route('/category/{id<\d+>}/delete', name: 'admin_category_delete', methods: ['POST'])]
+    public function delete(Category $category, Request $request): Response 
+    {
+        if ($this->isCsrfTokenValid('delete_category_'.$category->getId(), $request->request->get('_csrf_token')))
+        {
+          
+            $this->addFlash("success", "La catégorie {$category->getName()}a été supprimée");
+
+            $this->em->remove($category);
+            $this->em->flush();
+            
+        }
+
+        return $this->redirectToRoute("admin_category_index");
+    }
+
 }
